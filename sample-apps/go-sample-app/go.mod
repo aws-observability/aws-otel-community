9556c51f03ff810d5c9b64f221d4deb6328d992f
Alexis <perzmen@amazon.com>
2022-07-14 12:48:08 -0700

    Added counter and updown counter; TODO=fix updowncounter

--- sample-apps/go-sample-app/go.mod
+++ sample-apps/go-sample-app/go.mod
@@ -2,4 +2,33 @@ module github.com/aws-otel-commnunity/sample-apps/go-sample-app
 
 go 1.18
 
-require gopkg.in/yaml.v3 v3.0.1
+require (
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.31.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v0.31.0
+	go.opentelemetry.io/otel/metric v0.31.0
+	go.opentelemetry.io/otel/sdk/metric v0.31.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	go.opentelemetry.io/otel v1.8.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.8.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.8.0 // indirect
+	go.opentelemetry.io/otel/trace v1.8.0 // indirect
+	go.opentelemetry.io/proto/otlp v0.18.0 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220107163113-42d7afdf6368 // indirect
+	google.golang.org/grpc v1.46.2 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+)
--- sample-apps/go-sample-app/main.go
+++ sample-apps/go-sample-app/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/metric/global"
+	"go.opentelemetry.io/otel/metric/instrument"
+	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
+	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
+	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
+	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
 	"gopkg.in/yaml.v3"
 )
 
 var (
+	meter = global.MeterProvider().Meter("OTLP_METRIC_SAMPLE_APP")
 
 	// Request based metrics; values generated upon endpoint requests
 	totalRequests        string
@@ -18,7 +35,7 @@ var (
 	// Default values for random based metrics
 	defaultHost                    = "0.0.0.0"
 	defaultPort                    = "4567"
-	defaultTimeAliveInrementer     = 5
+	defaultTimeAliveInrementer     = 1
 	defaultTotalHeapSizeUpperBound = 100
 	defaultThreadsActiveUpperBound = 10
 	defaultCpuUsageUpperBound      = 100
@@ -37,7 +54,61 @@ type conf struct {
 func main() {
 	var c conf
 	c.getConf()
+	ctx := context.Background()
+	shutdown := startClient(ctx)
+	defer shutdown()
 
+	go updateLoop(ctx)
+	fmt.Println("Reporting measurements to locahost:3418...")
+	ch := make(chan os.Signal, 3)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
+	<-ch
+
+}
+
+// Function that creates and returns a New client with certain options
+// In this case we are sending insecure options (http instead of https)
+func otlpmetricClient(endpoint string) otlpmetric.Client {
+	options := []otlpmetrichttp.Option{
+		otlpmetrichttp.WithInsecure(),
+		otlpmetrichttp.WithEndpoint(endpoint),
+	}
+
+	return otlpmetrichttp.NewClient(options...)
+}
+
+func startClient(ctx context.Context) func() {
+	endpoint := os.Getenv("OTLP_EXPORTER_OTLP_ENDPOINT")
+	if endpoint == "" {
+		endpoint = "0.0.0.0:4318"
+	}
+	cumulativeSelector := aggregation.CumulativeTemporalitySelector()
+	metricExp, err := otlpmetric.New(ctx, otlpmetricClient(endpoint), otlpmetric.WithMetricAggregationTemporalitySelector(cumulativeSelector))
+	if err != nil {
+		//Logs here
+	}
+	ctrl := controller.New(
+		processor.NewFactory(
+			selector.NewWithHistogramDistribution(),
+			metricExp,
+		),
+		controller.WithExporter(metricExp),
+		controller.WithCollectPeriod(3*time.Second),
+	)
+	if err := ctrl.Start(ctx); err != nil {
+		// Logs here
+	}
+	global.SetMeterProvider(ctrl)
+	// Pass function to shutdown the controller in a defer statement
+	return func() {
+		cxt, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+
+		// pushes any last exports to the receiver
+		if err := ctrl.Stop(cxt); err != nil {
+			otel.Handle(err)
+		}
+	}
 }
 
 // Reads the config file and writes to the struct with the appropriate values
@@ -45,18 +116,18 @@ func (c *conf) getConf() *conf {
 	yamlFile, err := ioutil.ReadFile("config.yaml")
 	if err != nil {
 		//logs here
-		return c.validateConf()
+		return c.getDefaultConfig()
 	}
 	err = yaml.Unmarshal(yamlFile, c)
 	if err != nil {
 		//more logs here
-		return c.validateConf()
+		return c.getDefaultConfig()
 	}
 	return c
 }
 
 // Default to default values incase config file is missing
-func (c *conf) validateConf() *conf {
+func (c *conf) getDefaultConfig() *conf {
 	c.Host = defaultHost
 	c.Port = defaultPort
 	c.TimeAliveIncrementer = int64(defaultTimeAliveInrementer)
@@ -64,4 +135,33 @@ func (c *conf) validateConf() *conf {
 	c.ThreadsActiveUpperBound = int64(defaultThreadsActiveUpperBound)
 	c.CpuUsageUpperBound = int64(defaultCpuUsageUpperBound)
 	return c
+}
+
+func counterObserver(ctx context.Context) {
+	counter, _ := meter.SyncInt64().Counter(
+		"Time Alive",
+		instrument.WithUnit("s"),
+		instrument.WithDescription("Total time that the application has been alive for"),
+	)
+	counter.Add(ctx, 1)
+}
+
+func upDownCounterObserver(ctx context.Context) {
+	upDownCounter, _ := meter.SyncInt64().UpDownCounter(
+		"Threads Active",
+		instrument.WithUnit("1"),
+		instrument.WithDescription("Number of threads currently active"),
+	)
+	upDownCounter.Add(ctx, 1)
+}
+
+func updateLoop(ctx context.Context) {
+	go func() {
+		for {
+			upDownCounterObserver(ctx)
+			counterObserver(ctx)
+			time.Sleep(time.Second * 1)
+			log.Print("Updating TimeAlive...")
+		}
+	}()
 }
