54987979a1df5cb35fb39c90329c4c7ef0a73a3c
Alexis <perzmen@amazon.com>
2022-07-26 13:29:29 -0700

    trace otlp exporter

--- sample-apps/ruby-rails-sample-app/config/initializers/opentelemetry.rb
+++ sample-apps/ruby-rails-sample-app/config/initializers/opentelemetry.rb
@@ -1,5 +1,9 @@
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/all'
+
 OpenTelemetry::SDK.configure do |c|
-    c.service_name = 'aws-otel-manual-rails-sample'
+    c.service_name = 'ruby-sample-app'
   
     c.id_generator = OpenTelemetry::Propagator::XRay::IDGenerator
     c.propagators = [OpenTelemetry::Propagator::XRay::TextMapPropagator.new]
@@ -7,6 +11,6 @@ OpenTelemetry::SDK.configure do |c|
 
   
     # Alternatively, we could just enable all instrumentation:
-    c.use_all({ 'OpenTelemetry::Instrumentation::ActiveRecord' => { enabled: false } })
+    c.use_all()
   end
   
