2dc3427eabf3a46f2cc6457b5ed944cb1bb3619d
Nicholas Sulistio <sulisti@amazon.com>
2022-08-29 15:48:58 -0700

    changed name of directory

---
+++ sample-apps/python-auto-instrumented-sample-app/config.py
@@ -0,0 +1,17 @@
+import yaml
+
+def create_config(cfg):
+    config = {}
+    if cfg:
+        with open(cfg, 'r') as file:
+            config = yaml.safe_load(file)
+    config.setdefault('Host','0.0.0.0')
+    config.setdefault('Port','8080')
+    config.setdefault('TimeInterval',1)
+    config.setdefault('RandomTimeAliveIncrementer',1)
+    config.setdefault('RandomTotalHeapSizeUpperBound',100)
+    config.setdefault('RandomThreadsActiveUpperBound',10)
+    config.setdefault('RandomCpuUsageUpperBound',100)
+    config.setdefault('SampleAppPorts',['8080'])
+    return config
+
