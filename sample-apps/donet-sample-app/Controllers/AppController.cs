407eecb65f2bd9ad545d4666cf322e4196fdee2a
Kausik <kausika@amazon.com>
2023-02-10 15:52:11 +0000

    Moving dir structure

--- sample-apps/donet-sample-app/Controllers/AppController.cs
+++ sample-apps/donet-sample-app/Controllers/AppController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http.Extensions;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Instrumentation;
+using System.Diagnostics.Metrics;
 
 namespace integration_test_app.Controllers
 {
@@ -15,30 +19,50 @@ namespace integration_test_app.Controllers
         private readonly HttpClient httpClient = new HttpClient();
         private readonly MetricEmitter _metricEmitter = new MetricEmitter();
         private static Random rand = new Random(DateTime.Now.Millisecond);
+        
 
+        public AppController()
+        {
+            //Random Metrics
+            _metricEmitter.updateTotalTimeMetric(rand.Next(10,30));
+            _metricEmitter.updateTotalHeapSizeMetric(rand.Next(100,500));
+            _metricEmitter.updateTotalThreadSizeMetric(rand.Next(1,20));
+            _metricEmitter.updateCpuUsageMetric(rand.Next(1,100));
+        }
+
+        
         [HttpGet]
         [Route("/outgoing-http-call")]
         public string OutgoingHttp()
         {
             var res = httpClient.GetAsync("https://aws.amazon.com").Result;
             string statusCode = res.StatusCode.ToString();
-            // mimic latency for now
-            _metricEmitter.emitReturnTimeMetric(MimicLatency(),Request.GetDisplayUrl(),statusCode);
             
-            // emit load size
+            // Request Based Metrics
+            _metricEmitter.emitReturnTimeMetric(MimicLatency(),Request.GetDisplayUrl(),statusCode);
             int loadSize = MimicPayLoadSize();
-            Console.WriteLine(loadSize);
-            _metricEmitter.emitBytesSentMetric(loadSize,Request.GetDisplayUrl(),statusCode);
+            _metricEmitter.apiRequestSentMetric(Request.GetDisplayUrl(),statusCode);
             _metricEmitter.updateTotalBytesSentMetric(loadSize, Request.GetDisplayUrl(),statusCode);
+
             
             return GetTraceId();
         }
 
         [HttpGet]
-        [Route("/get-aws-s3-buckets")]
+        [Route("/aws-sdk-call")]
         public string AWSSDKCall()
         {
-            _ = s3Client.ListBucketsAsync().Result;
+            var res = s3Client.ListBucketsAsync().Result;
+            string statusCode = res.HttpStatusCode.ToString();
+            
+            // Request Based Metrics
+            _metricEmitter.emitReturnTimeMetric(MimicLatency(),Request.GetDisplayUrl(),statusCode);
+            int loadSize = MimicPayLoadSize();
+            _metricEmitter.apiRequestSentMetric(Request.GetDisplayUrl(),statusCode);
+            _metricEmitter.updateTotalBytesSentMetric(loadSize, Request.GetDisplayUrl(),statusCode);
+
+
+
 
             return GetTraceId();
         }
@@ -47,7 +71,7 @@ namespace integration_test_app.Controllers
         [Route("/")]
         public string Default()
         {
-            return "Application has started!";
+            return "Application started!";
         }
 
         private string GetTraceId()
@@ -61,12 +85,12 @@ namespace integration_test_app.Controllers
 
         private static int MimicPayLoadSize()
         {
-            return rand.Next(107);
+            return rand.Next(101);
         }
 
         private static int MimicLatency()
         {
-            return rand.Next(90,5000);
+            return rand.Next(100,500);
         }
     }
 
---
+++ sample-apps/donet-sample-app/Controllers/MetricEmitter.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using OpenTelemetry;
+using System.Diagnostics.Metrics;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Instrumentation;
+
+namespace integration_test_app.Controllers
+{
+    public class MetricEmitter
+    {
+        const string DIMENSION_API_NAME = "apiName";
+        const string DIMENSION_STATUS_CODE = "statusCode";
+        
+
+        static string API_COUNTER_METRIC = "totalApiRequests";
+        static string API_LATENCY_METRIC = "latencyTime";
+        static string API_SUM_METRIC = "totalBytesSent";
+        static string API_TOTAL_TIME_METRIC = "timeAlive";
+        static string API_TOTAL_HEAP_SIZE = "heapSize";
+        static string API_TOTAL_THREAD_SIZE = "threadSize";
+        static string API_CPU_USAGE = "cpuUsage";
+    
+
+        Histogram<double> apiLatencyRecorder;
+        Counter<long> totalTimeSentObserver;
+
+        static long apiRequestSent = 0;
+        static long totalBytesSent = 0;
+        static long totalHeapSize  = 0;
+        static long totalThreadSize = 0;
+        static int cpuUsage = 0;
+
+        // The below API name and status code dimensions are currently shared by all metrics observer in
+        // this class.
+        string apiNameValue = "";
+        string statusCodeValue = "";
+
+        
+        public MetricEmitter()
+        {
+            Meter meter = new Meter("aws-otel", "1.0");
+            using var meterProvider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter("aws-otel")
+                .AddOtlpExporter()
+                .Build();
+            
+            string latencyMetricName = API_LATENCY_METRIC;
+            string totalApiRequestSent = API_COUNTER_METRIC;
+            string totalApiBytesSentMetricName = API_SUM_METRIC;
+            string totaltimealiveMetricName = API_TOTAL_TIME_METRIC;
+            string totalHeapSizeMetricName = API_TOTAL_HEAP_SIZE;
+            string totalThreadsMetricName = API_TOTAL_THREAD_SIZE;
+            string cpuUsageMetricName = API_CPU_USAGE;
+
+
+            string instanceId = Environment.GetEnvironmentVariable("INSTANCE_ID");
+            if (instanceId != null && !instanceId.Trim().Equals(""))
+            {
+                latencyMetricName = API_LATENCY_METRIC + "_" + instanceId;
+                totalApiRequestSent = API_COUNTER_METRIC + "_" + instanceId;
+                totalApiBytesSentMetricName = API_SUM_METRIC + "_" + instanceId;
+                totaltimealiveMetricName = API_TOTAL_TIME_METRIC + "_" + instanceId;
+                totalHeapSizeMetricName = API_TOTAL_HEAP_SIZE + "_" + instanceId;
+                totalThreadsMetricName = API_TOTAL_THREAD_SIZE + "_" + instanceId;
+                cpuUsageMetricName = API_CPU_USAGE + "_" + instanceId;
+    
+            }
+            
+
+            meter.CreateObservableCounter(totalApiRequestSent,() => apiRequestSent, 
+                "1",
+                "Increments by one every time a sampleapp endpoint is used");
+
+            meter.CreateObservableCounter(totalApiBytesSentMetricName, () => totalBytesSent, 
+                "By",
+                "Keeps a sum of the total amount of bytes sent while the application is alive");
+
+            apiLatencyRecorder = meter.CreateHistogram<double>(latencyMetricName,
+                 "ms", 
+                 "Measures latency time in buckets of 100 300 and 500");
+
+            totalTimeSentObserver = meter.CreateCounter<long>(totaltimealiveMetricName,
+                "ms",
+                "Measures the total time the application has been alive");
+
+            meter.CreateObservableUpDownCounter(totalHeapSizeMetricName, () => totalHeapSize, 
+                "By",
+                "The current total heap size”");  
+
+            meter.CreateObservableCounter(totalThreadsMetricName, () => totalThreadSize,
+                "1",
+                "The current total number of threads”");
+
+            meter.CreateObservableGauge(cpuUsageMetricName, () => cpuUsage,
+                "1",
+                "Cpu usage percent");
+
+        }   
+        
+        public void emitReturnTimeMetric(long returnTime, String apiName, String statusCode) {
+            apiLatencyRecorder.Record(
+                returnTime,
+                new KeyValuePair<string, object>(DIMENSION_API_NAME, apiName),
+                new KeyValuePair<string, object>(DIMENSION_STATUS_CODE, statusCode));
+        }
+
+        public void apiRequestSentMetric(String apiName, String statusCode) {
+                apiRequestSent += 1;
+                Console.WriteLine("apiBs: "+ apiRequestSent);  
+        } 
+    
+        public void updateTotalBytesSentMetric(int bytes, String apiName, String statusCode) {
+            totalBytesSent += bytes;
+            Console.WriteLine("Total amount of bytes sent while the application is alive:"+ totalBytesSent);
+            apiNameValue = apiName;
+            statusCodeValue = statusCode;
+        }
+
+        public void updateTotalHeapSizeMetric(int heapSize) {
+            totalHeapSize += heapSize;
+        }
+
+        public void updateTotalThreadSizeMetric(int threadSize) {
+            totalThreadSize += threadSize;
+        }
+
+        public void updateCpuUsageMetric(int cpuUsage) {
+            cpuUsage = cpuUsage;
+        }
+
+        public void updateTotalTimeMetric(int totaltime) {
+           totalTimeSentObserver.Add(totaltime);
+        }
+
+    }
+}
